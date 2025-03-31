b9f074807c911e0893ec098f8b50a9dd27b5b16b
NguyenBao2110 <ngb37200100@gmail.com>
2025-03-31 12:59:01 +0700

    Add files via upload

---
+++ Queue.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef struct
+{
+	int front;
+	int rear;
+	int count;
+	int size;
+	int* Arr;
+}Queue;
+void Init(Queue& q, int size)
+{
+	q.front = -1;
+	q.rear = -1;
+	q.size = size;
+	q.Arr = new int[q.size];
+}
+void Delete(Queue& q)
+{
+	q.front = q.rear = -1;
+	q.count = 0;
+	q.size = 0;
+	delete[] q.Arr;
+}
+bool IsEmpty(Queue& q)
+{
+	return q.front == -1;
+}
+bool IsFull(Queue& q)
+{
+	return (q.rear - q.front+1 == 0) || (q.rear - q.front+1 == q.size );
+}
+void EnQueue(Queue& q, int x)
+{
+	if (q.front == -1)
+		q.front = 0;
+	q.rear++;
+	if (q.rear == q.size)
+		q.rear = 0;
+	q.Arr[q.rear] = x;
+	q.count++;
+}
+int DeQueue(Queue& q)
+{
+	int x = q.Arr[q.front];
+	if (q.front == q.rear)
+	{
+		q.front = -1;
+		q.rear = -1;
+	}
+	else
+	{
+		q.front++;
+		if (q.front == q.size)
+			q.front = 0;
+	}
+	q.count--;
+	return x;
+}
+int main()
+{
+	Queue Q;
+	int n; cin >> n;
+	Init(Q, n);
+	for (int i = 0; i < n; i++) 
+	{
+		EnQueue(Q, i);
+	}
+	while (!IsEmpty(Q))
+	{
+		cout << DeQueue(Q);
+	}
+	return 0;
+}
